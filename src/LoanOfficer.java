3bb41e384ab1b778503c024ee3ec1135f0ec2aa7
Kevin Chen <ningchen227@gmail.com>
2019-10-03 17:10:01 -0500

    First commit

---
+++ src/LoanOfficer.java
@@ -0,0 +1,10 @@
+/**
+ * Processes customer request and updates customer balance.
+ *
+ *@author Kevin Chen
+ *@version 1.0
+*/
+
+public class LoanOfficer {
+
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Creates and joins all customer threads.
+ * When last customer has exited, prints the summary report and ends the simulation.
+ *
+ *@author Kevin Chen
+ *@version 1.0
+*/
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
